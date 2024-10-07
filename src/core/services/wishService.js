7026571c940eec9e505790b6c6d4ce735c0fccc4
Nhigtma <julianestebanacunasalazar@gmail.com>
2024-10-07 02:27:11 -0500

    genrear y descargar

--- src/core/services/wishService.js
+++ src/core/services/wishService.js
@@ -3,9 +3,6 @@ const RoutineWishRepository = require('../../adapters/outbound/routineWishReposi
 const routineWishService = require('../../core/services/routineWishesService');
 const UserPointsService = require('../services/userPointsService');
 const HistoryService = require('../services/historyServices');
-const fs = require('fs');
-const PDFDocument = require('pdfkit');
-const path = require('path');
 
 
 class WishService {
@@ -89,11 +86,11 @@ class WishService {
     async getWishesWithLists(user_id) {
         try {
             const wishes = await this.wishRepository.getAllWisheslist(user_id);
-            
+
             if (!wishes || wishes.length === 0) {
                 return [];
             }
-    
+
             const wishesWithRoutines = await Promise.all(wishes.map(async (wish) => {
                 if (wish.is_routine) {
                     const routines = await this.RoutineWishRepository.getRoutinesByWishId(wish.id);
@@ -104,7 +101,7 @@ class WishService {
                 }
                 return wish;
             }));
-    
+
             return wishesWithRoutines;
         } catch (error) {
             console.error('Error en getWishesWithLists', error);
@@ -169,14 +166,14 @@ class WishService {
     async resetWasPerformed() {
         const wishes = await this.wishRepository.getWishesWithLists();
         let updated = false;
-    
+
         for (const wish of wishes) {
             const updatedWish = await this.wishRepository.updateWasPerformed(wish.id);
             if (updatedWish) {
                 updated = true;
             }
         }
-    
+
         return updated ? 'Rutinas actualizadas correctamente.' : 'No se encontraron rutinas que actualizar.';
     }
 
@@ -194,7 +191,7 @@ class WishService {
     async getWishByCategory(category_id) {
         try {
             const wishes = await this.wishRepository.getWishesByCategory(category_id);
-            console.log (wishes);
+            console.log(wishes);
             return wishes;
         } catch (error) {
             console.error('Error en getWishesByCategory', error);
@@ -202,71 +199,6 @@ class WishService {
         }
     }
 
-    async generatePDF(req, res) {
-        const userId = req.params.userId;
-        const dias = {
-            LUNES: "3eaf730d-79f4-481b-9779-61f05370bf94",
-            MARTES: "db467a33-3486-439a-9a26-11864fb55be6",
-            MIERCOLES: "004e5c38-9266-4eb0-b71b-05b462c24d30",
-            JUEVES: "6c5d6e27-8594-4999-8129-5a618c869ce5",
-            VIERNES: "c53082d3-12a5-4f0d-b794-ce4fa8d8307e",
-            SABADO: "44079d90-eb72-4216-a887-fc8e130f8ea8",
-            DOMINGO: "d03406b9-ec67-45aa-8454-3e848ac3fcbf"
-        };
-    
-        const weekOrder = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO"];
-        const wishesWithRoutines = await this.getWishesWithLists(userId);
-        
-        const routinesByDay = {};
-    
-        wishesWithRoutines.forEach(wish => {
-            if (wish.is_routine) {
-                const routines = wish.routines;
-                routines.forEach(routine => {
-                    const dayKey = Object.keys(dias).find(day => dias[day] === routine.week_day_id);
-                    if (dayKey) {
-                        if (!routinesByDay[dayKey]) {
-                            routinesByDay[dayKey] = [];
-                        }
-                        routinesByDay[dayKey].push({ wishTitle: wish.title });
-                    }
-                });
-            }
-        });
-    
-        const doc = new PDFDocument();
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', `attachment; filename=routines_${userId}.pdf`);
-    
-        doc.pipe(res);
-    
-        doc.fontSize(25).text('Rutinas de la semana', { align: 'center' });
-        doc.moveDown();
-    
-        let dayIndex = 0;
-        let totalRoutines = 0;
-        Object.values(routinesByDay).forEach(routines => {
-            totalRoutines += routines.length;
-        });
-    
-        let printedRoutines = 0;
-        while (printedRoutines < totalRoutines) {
-            const currentDay = weekOrder[dayIndex % weekOrder.length];
-            const routines = routinesByDay[currentDay];
-            if (routines && routines.length > 0) {
-                doc.fontSize(20).text(currentDay);
-                doc.fontSize(14).text('Rutinas:');
-                routines.forEach(routine => {
-                    doc.text(`- ${routine.wishTitle}`);
-                    printedRoutines++;
-                });
-                doc.moveDown();
-            }
-            dayIndex++;
-        }
-    
-        doc.end();
-    }
 }
 
 module.exports = WishService;
--- src/ports/routineWishesController.js
+++ src/ports/routineWishesController.js
@@ -1,5 +1,8 @@
 const RoutineWishService = require('../core/services/routineWishesService');
 const WishService = require('../core/services/wishService');
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+const path = require('path');
 
 class RoutineWishController {
     constructor() {
@@ -9,7 +12,7 @@ class RoutineWishController {
 
     async createWishWithRoutine(req, res) {
         const { title, description, user_id, category_id, routines, is_routine } = req.body;
-    
+
         try {
             console.log('Datos recibidos:');
             console.log('Title:', title);
@@ -18,11 +21,11 @@ class RoutineWishController {
             console.log('Category ID:', category_id);
             console.log('Routines:', routines);
             console.log('Is Routine:', is_routine);
-    
+
             if (!title || !description || !user_id || !category_id || !routines || !is_routine) {
                 return res.status(400).json({ message: "Faltan datos requeridos." });
             }
-    
+
             const result = await this.wishService.createWishWithRoutine(
                 title,
                 description,
@@ -31,13 +34,13 @@ class RoutineWishController {
                 routines,
                 is_routine
             );
-    
+
             return res.status(201).json({
                 message: "Deseo creado con éxito",
                 wish: result.wish,
                 routines: result.routines
             });
-    
+
         } catch (error) {
             console.error('Error en createWishWithRoutine:', error.message);
             return res.status(500).json({ message: "Error al crear el deseo con rutina: " + error.message });
@@ -57,11 +60,11 @@ class RoutineWishController {
 
     async getWishesWithLists(req, res) {
         const { user_id } = req.params;
-    
+
         if (!user_id) {
             return res.status(400).json({ message: "El user_id es requerido." });
         }
-    
+
         try {
             const wishesWithLists = await this.wishService.getWishesWithLists(user_id);
             return res.status(200).json(wishesWithLists);
@@ -95,27 +98,69 @@ class RoutineWishController {
     }
 
     async generatePDF(req, res) {
-        // Asegúrate de que req.params existe
-        console.log(req.params); // Añade esto para depurar
-
-        const { userId } = req.params; // Aquí es donde se genera el error si userId no existe
-
-        if (!userId) {
-            return res.status(400).json({ error: 'El userId es requerido.' });
+        const userId = req.params.userId;
+        const dias = {
+            LUNES: "3eaf730d-79f4-481b-9779-61f05370bf94",
+            MARTES: "db467a33-3486-439a-9a26-11864fb55be6",
+            MIERCOLES: "004e5c38-9266-4eb0-b71b-05b462c24d30",
+            JUEVES: "6c5d6e27-8594-4999-8129-5a618c869ce5",
+            VIERNES: "c53082d3-12a5-4f0d-b794-ce4fa8d8307e",
+            SABADO: "44079d90-eb72-4216-a887-fc8e130f8ea8",
+            DOMINGO: "d03406b9-ec67-45aa-8454-3e848ac3fcbf"
+        };
+    
+        const weekOrder = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO"];
+        const wishesWithRoutines = await this.wishService.getWishesWithLists(userId);
+        
+        const routinesByDay = {};
+    
+        wishesWithRoutines.forEach(wish => {
+            if (wish.is_routine) {
+                const routines = wish.routines;
+                routines.forEach(routine => {
+                    const dayKey = Object.keys(dias).find(day => dias[day] === routine.week_day_id);
+                    if (dayKey) {
+                        if (!routinesByDay[dayKey]) {
+                            routinesByDay[dayKey] = [];
+                        }
+                        routinesByDay[dayKey].push({ wishTitle: wish.title });
+                    }
+                });
+            }
+        });
+    
+        const doc = new PDFDocument();
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename=routines_${userId}.pdf`);
+    
+        doc.pipe(res);
+    
+        doc.fontSize(25).text('Rutinas de la semana', { align: 'center' });
+        doc.moveDown();
+    
+        let dayIndex = 0;
+        let totalRoutines = 0;
+        Object.values(routinesByDay).forEach(routines => {
+            totalRoutines += routines.length;
+        });
+    
+        let printedRoutines = 0;
+        while (printedRoutines < totalRoutines) {
+            const currentDay = weekOrder[dayIndex % weekOrder.length];
+            const routines = routinesByDay[currentDay];
+            if (routines && routines.length > 0) {
+                doc.fontSize(20).text(currentDay);
+                doc.fontSize(14).text('Rutinas:');
+                routines.forEach(routine => {
+                    doc.text(`- ${routine.wishTitle}`);
+                    printedRoutines++;
+                });
+                doc.moveDown();
+            }
+            dayIndex++;
         }
-
-        try {
-            const pdfPath = await this.wishService.generatePDF(userId);
-            res.download(pdfPath, (err) => {
-                if (err) {
-                    console.error('Error al enviar el PDF:', err);
-                    res.status(500).send('Error al generar el PDF');
-                }
-            });
-        } catch (error) {
-            console.error('Error al generar el PDF:', error);
-            res.status(500).send('Error al generar el PDF');
-        }
+    
+        doc.end();
     }
 }
 
